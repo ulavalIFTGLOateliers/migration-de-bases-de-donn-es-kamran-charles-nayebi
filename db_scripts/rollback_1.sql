4d5f8f2480d33a03e70d3c37dcd16886bb6852d0
Kamran Charles Nayebi <2135433@cegeplimoilou.ca>
2024-03-31 19:19:16 -0400

    Commit final

--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,4 +1,5 @@
 DROP TABLE band;
 ALTER TABLE musician RENAME COLUMN musicianName TO singerName;
 ALTER TABLE musician RENAME TO singer;
-
+ALTER TABLE singer DROP COLUMN role;
+ALTER TABLE singer DROP COLUMN bandName;
